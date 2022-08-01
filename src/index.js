e9e295561155dd99208e0dcc8bfdc108ed699c7d
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-08-01 11:15:45 +0200

    Move code from index to app and vice versa

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./App.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import WeatherApp from "./WeatherApp";
+
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <WeatherApp city="New York" />
   </React.StrictMode>
 );
 
