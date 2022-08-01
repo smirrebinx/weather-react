359b36fdaac3070d661c392a25f766c78cf7ca37
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-08-01 17:15:14 +0200

    Delete property

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <WeatherApp city="New York" />
+        <WeatherApp />
       </div>
     </div>
   );
