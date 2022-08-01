0c12a6340075ede6ee7dac61cae89ab0181c8481
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-08-01 17:49:03 +0200

    Add background image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import WeatherApp from "./WeatherApp";
 import "./App.css";
+import background from "./images/background.jpg";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
+      <div
+        className="container"
+        style={{ backgroundImage: `url(${background})` }}
+      >
         <WeatherApp />
       </div>
     </div>
