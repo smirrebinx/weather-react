b2cd01b6780327c36248390d5922cd23be27c330
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-08-28 13:14:52 +0200

    Fix background image
    Background images takes up all the space

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
 import WeatherApp from "./WeatherApp";
 import "./App.css";
-import background from "./images/background.jpg";
 
 export default function App() {
   return (
     <div className="App">
-      <div
-        className="container"
-        style={{ backgroundImage: `url(${background})` }}
-      >
-        <WeatherApp />
-      </div>
+      <WeatherApp />
     </div>
   );
 }
