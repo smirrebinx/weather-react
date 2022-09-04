aa1bfbd974742e1f1a9191098f9427db632ae03e
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-09-04 12:06:41 +0200

    Update footer with links to linkedin and github

--- src/Footer.js
+++ src/Footer.js
@@ -2,21 +2,54 @@ import React from "react";
 import a_cat from "./images/a_cat.jpg";
 
 export default function Footer() {
+  let urlLinkedin = "https://www.linkedin.com/in/michellewegler/";
+  let urlGithub = "https://github.com/smirrebinx/weather-react.git";
+
   return (
     <div className="App">
-      <footer className="footer">
-        <div className="logo">
-          <img src={a_cat} alt="Logo, black cat" />
-          <small>
-            <a
-              href="https://github.com/smirrebinx/weather-react.git"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Open source code
-            </a>
-            <span className="logoTextSpan">by Michelle Wegler</span>
-          </small>
+      <footer>
+        <div className="container">
+          <div className="footer-container">
+            <div className="d-flex justify-content-center">
+              <div className="ml-5 mt-3">
+                <img
+                  src={a_cat}
+                  alt="Logo, black cat"
+                  title="Logo, black cat"
+                  className="logo"
+                />
+                <span className="logoTextSpan ml-3 mr-5">
+                  Michelle Wegler 2022, open source
+                </span>
+              </div>
+            </div>
+            <div className="d-flex justify-content-center">
+              <div className="d-flex mt-3 mb-3">
+                <a
+                  href={urlLinkedin}
+                  className="link"
+                  title="Linkedin Profile"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-brands fa-linkedin" aria-hidden="true"></i>
+                  <span className="sr-only">Linkedin</span>
+                </a>
+              </div>
+              <div className="d-flex mr-5 mt-3 mb-3">
+                <a
+                  href={urlGithub}
+                  className="link"
+                  title="Github Profile"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-github" aria-hidden="true"></i>
+                  <span className="sr-only">Github</span>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </footer>
     </div>
