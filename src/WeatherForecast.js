b3297e100e6fc9506aa4ed8d2ea52c6add8e8642
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2022-09-05 09:46:16 +0200

    Shorten apiUrl by adding variables

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,9 +17,11 @@ export default function WeatherForecast(props) {
 
   function load() {
     let apiKey = "88ee6169873d6d2e07af4c35247ba163";
+    let apiEndpoint = `https://api.openweathermap.org/data/2.5/onecall?`;
+    let unit = `metric`;
     let lon = props.coordinates.lon;
     let lat = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    let apiUrl = `${apiEndpoint}lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
 
     axios.get(apiUrl).then(handleResponse);
   }
